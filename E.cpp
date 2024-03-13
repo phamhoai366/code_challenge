2c0802d91b1192f501a2fc5bb70550ff424f18b0
Pham Hoai <101795746+phamhoai366@users.noreply.github.com>
2024-03-13 16:18:57 +0700

    Update E.cpp

--- E.cpp
+++ E.cpp
@@ -1,8 +1,30 @@
-#include <iostream>
-#include <climits>
-#include <stdlib.h>
-#include <queue>
+#include<iostream>
+#include<stdlib.h>
+#include<climits>
+#include<queue>
+
 using namespace std;
+
+void delFirst(int arr[], int n){
+    for(int i = 0; i < n-1; i++){
+        arr[i] = arr[i+1];
+    }
+}
+
+void delLast(int arr[], int n){
+    arr[n-1] = 0;
+}
+
+void addFirst(int arr[], int n, int x){
+    for(int i = n; i > 0; i--){
+        arr[i] = arr[i-1];
+    }
+    arr[0] = x;
+}
+
+void AddLast(int arr[], int n, int x){
+    arr[n] = x;
+}
 
 int maxSubarraySum(int arr[], int n) {
     int maxSum = INT_MIN;
@@ -17,50 +39,7 @@ int maxSubarraySum(int arr[], int n) {
             currentSum = 0;
         }
     }
-
     return maxSum;
-}
-
-// Inserting an element at the beginning of the array
-void AddBegin(int c[], int size, int n){
-    int newSize = size + 1;
-    int newC[newSize];
-
-    newC[0] = n;
-    for(int j = 0; j < size; j++){
-    newC[j + 1] = c[j];
-    }
-}
-
-// Inserting an element at the end of the array
-void AddEnd(int c[], int size, int n){
-    int newSize = size + 1;
-    int newC[newSize];
-
-    for(int j = 0; j < size; j++){
-        newC[j] = c[j];
-    }
-    newC[newSize - 1] = n;
-}
-
-// Deleting an element from the beginning of the array
-void DelBegin(int c[], int size){
-    int newSize = size - 1;
-    int newC[newSize];
-
-    for(int j = 0; j < newSize; j++){
-        newC[j] = c[j + 1];
-    }
-}
-
-// Deleting an element from the end of the array
-void DelEnd(int c[], int size){
-    int newSize = size - 1;
-    int newC[newSize];
-
-    for(int j = 0; j < newSize; j++){
-        newC[j] = c[j];
-    }
 }
 
 int main() {
@@ -90,26 +69,27 @@ int main() {
         }
     }
 
-    int size;
-    int array[size];
-
     while(!q.empty()){
         int a = q.front();
         q.pop();
-        int b = q.front();
-        q.pop();
-        size = a + b;
 
         int c[a];
         for(int i = 0; i < a; i++){
             c[i] = q1.front();
             q1.pop();
         }
-        int d[b];
-        for(int i = 0; i < b; i++){
+        int m = q.front();
+        q.pop();
+        int d[m];
+        for(int i = 0; i < m; i++){
             d[i] = q2.front();
             q2.pop();
         }
+
+        // int *arr = new int[a+m];
+        // for(int i = 0; i < a; i++){
+        //     arr[i] = c[i];
+        // }
 
         int sum1 = 0, sum2 = 0;
 
@@ -129,77 +109,73 @@ int main() {
                 sum2 += c[i];
             }
         }
-
-        if(sum1 > sum2){
-            if(d[0] > d[b-1]){
-                if(d[0] > 0){
-                    AddBegin(c, a, d[0]);
-                    a += 1;
-                    DelBegin(d, b);
-                    b -= 1;
+        while(m != 0){
+            if(sum1 > sum2){
+                if(d[0] > d[m-1]){
+                    if(d[0] > 0){
+                        addFirst(c, a, d[0]);
+                        a += 1; 
+                        delFirst(d, m);
+                        m -= 1;
+                    }
+                    else{
+                        AddLast(c, a, d[0]);
+                        a += 1;
+                        delFirst(d, m);
+                        m -= 1;
+                    }
                 }
                 else{
-                    AddEnd(c, a, d[b-1]);
-                    a += 1;
-                    DelBegin(d, b);
-                    b -= 1;
+                    if(d[m-1] > 0){
+                        AddLast(c, a, d[m-1]);
+                        a += 1;
+                        delLast(d, m);
+                        m -= 1;
+                    }
+                    else{
+                        addFirst(c, a, d[m-1]);
+                        a += 1;
+                        delLast(d, m);
+                        m -= 1;
+                    }
                 }
             }
             else{
-                if(d[b-1] > 0){
-                    AddEnd(c, a, d[b-1]);
-                    a += 1;
-                    DelEnd(d, b);
-                    b -= 1;
+                if(d[0] > d[m-1]){
+                    if(d[0] > 0){
+                        AddLast(c, a, d[0]);
+                        a += 1;
+                        delFirst(d, m);
+                        m -= 1;
+                    }
+                    else{
+                        addFirst(c, a, d[0]);
+                        a += 1;
+                        delFirst(d, m);
+                        m -= 1;
+                    }
                 }
                 else{
-                    AddBegin(c, a, d[0]);
-                    a += 1;
-                    DelEnd(d, b);
-                    b -= 1;
+                    if(d[m-1] > 0){
+                        AddLast(c, a, d[m-1]);
+                        a += 1;
+                        delLast(d, m);
+                        m -= 1;
+                    }
+                    else{
+                        addFirst(c, a, d[m-1]);
+                        a += 1;
+                        delLast(d, m);
+                        m -= 1;
+                    }
                 }
             }
-        }
-        else{
-            if(d[0] > d[b-1]){
-                if(d[0] > 0){
-                    AddEnd(c, a, d[0]);
-                    a += 1;
-                    DelBegin(d, b);
-                    b -= 1;
-                }
-                else{
-                    AddBegin(c, a, d[b -1]);
-                    a += 1;
-                    DelBegin(d, b);
-                    b -= 1;
-                }
-            }
-            else{
-                if(d[b-1] > 0){
-                    AddEnd(c, a, d[b-1]);
-                    a += 1;
-                    DelEnd(d, b);
-                    b -= 1;
-                }
-                else{
-                    AddEnd(c, a, d[b-1]);
-                    a += 1;
-                    DelEnd(d, b);
-                    b -= 1;
-                }
-            }
-        }
-        
-        for(int i = 0; i < size; i++){
-            array[i] = c[i];
-            cout << c[i] << " ";
-        }
-    }
 
-
-    int maxSum = maxSubarraySum(array, size);
-    cout << maxSum << endl;
-    
+            for(int i = 0; i < a; i++){
+                    cout << c[i] << " ";
+            }
+            cout << endl;
+            }
+        }
     return 0;
 }
